d55046b4b492302f421b2aeed2a97a6e953d1dc5
Maria Arsuaga Rios <mariayh1@gmail.com>
2016-06-16 16:56:20 +0200

    adding main with import from github.com

---
+++ packages/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mariayh1/go-training/packages/stringutils"
+)
+
+func main() {
+	fmt.Println(stringutils.Reverse("Hola"))
+	fmt.Println(stringutils.MyName)
+}
