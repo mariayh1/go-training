2af1a339f01f60726d2e3c5ef61aac55a6b83680
Maria Arsuaga Rios <mariayh1@gmail.com>
2016-06-16 17:16:55 +0200

    adding variables

---
+++ variables/shorthand/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var b string
+	a := 10
+	d, f := true, "a"
+
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", d)
+	fmt.Printf("%T \n", a)
+	fmt.Printf("%T \n", d)
+	fmt.Printf("%v \n", b)
+	fmt.Println(a, b, d, f)
+}
